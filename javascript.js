8bb83d44a779225621182f3d1a9917a8785e28a4
Klaranaslund <naslundklara@gmail.com>
2023-09-26 13:29:50 +0200

    Revise comments in javascript.js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
-//funktion som slumpar rock paper eller scissors ochs kriver ut detta
+/* Rock Paper Scissors game with computer, to be played in the  */
 
+
+//function to get a randomized choice of rock paper or scissors from the computer
 function getComputerChoice(){
     //Array of strings to be randomized
     const strings = ["rock", "paper", "scissors"];
@@ -16,25 +18,29 @@ function getPlayerChoice(){
     return choice.toLowerCase();
   }
 
-  function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log("It's A tie");
-        return 0;
+//function to compare computer and player selection and thus decide who wins a single round. 
+//Returns 0 for a tie, 1 if player wins, -1 if computer wins.
+function playRound(playerSelection, computerSelection) {
+   if (playerSelection === computerSelection) {
+       console.log("It's A tie");
+       return 0;
 
-    } else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
-    ) {
-        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
-        return 1;
-    } else {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        return -1;
-    }
+   } else if (
+       (playerSelection == "rock" && computerSelection == "scissors") ||
+       (playerSelection == "scissors" && computerSelection == "paper") ||
+       (playerSelection == "paper" && computerSelection == "rock")
+   ) {
+       console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+       return 1;
+   } else {
+       console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+       return -1;
+   }
 }
 
 
+/*game function that loops over function playRound() until computer or player
+reaches a score of three, rather than a best of five because of ties*/
 function game() {
     let score = 0;
     while (score < 3 && score > -3) {
@@ -47,6 +53,3 @@ function game() {
         console.log("You lose!");
     }
 }
-
-//funktion game
-// spela 5 rundor som räknar poäng och deklarerar vinnare eller förlorare vid slutet
